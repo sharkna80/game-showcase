27796d5fd4c2667b3b5af6159161f06ff64821d3
Sharath Koday <sharath1608@gmail.com>
2023-06-01 19:49:27 -0700

    Added detail component with styling

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Game} from "../../models";
+import {ActivatedRoute, Route} from "@angular/router";
+import {HttpService} from "../../services/http.service";
+import {Subject, Subscription, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-game-details',
   templateUrl: './game-details.component.html',
   styleUrls: ['./game-details.component.scss']
 })
-export class GameDetailsComponent implements OnInit {
+export class GameDetailsComponent implements OnInit, OnDestroy {
+  gaugeType = "semi";
+  label = "rating";
+  rating: number = 20;
 
-  constructor() { }
+  game: Game | undefined;
+  gameId: string | null = '';
+  unsub: Subject<void> = new Subject<void>();
 
-  ngOnInit(): void {
+  thresholdConfig = {
+    '0': {color: '#ef4655'},
+    '30': {color: '#f7aa38'},
+    '50': {color: '#fffa50'},
+    '75': {color: '#5ee432'}
+  };
+
+  constructor(private route:ActivatedRoute, private httpService: HttpService) {
+
   }
 
+  ngOnInit(): void {
+
+    this.route.paramMap.subscribe((params) => {
+        this.gameId = params.get('id');
+        if(this.gameId) {
+          this.httpService.getGameDetails(this.gameId).pipe(takeUntil(this.unsub)).subscribe(res => {
+            if(res) {
+              this.game = res;
+            }
+          });
+        }
+      })
+  }
+
+  ngOnDestroy() {
+    this.unsub.next();
+  }
 }
