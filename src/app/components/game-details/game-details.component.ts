ba3d52f72c48ac4be3e31b48161905048bcdfb24
Sharath Koday <sharath1608@gmail.com>
2023-06-15 11:32:36 -0700

    Fetch details and assets

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Game} from "../../models";
+import {ApiResponse, Game, Screenshot, Trailer} from "../../models";
 import {ActivatedRoute, Route} from "@angular/router";
 import {HttpService} from "../../services/http.service";
-import {Subject, Subscription, takeUntil} from "rxjs";
+import {forkJoin, map, Observable, Subject, Subscription, takeUntil} from "rxjs";
+import {GameTabData} from "../game-tabs/game-tabs.component";
 
 @Component({
   selector: 'app-game-details',
@@ -15,6 +16,8 @@ export class GameDetailsComponent implements OnInit, OnDestroy {
   rating: number = 20;
 
   game: Game | undefined;
+  tabsData: GameTabData | undefined;
+
   gameId: string | null = '';
   unsub: Subject<void> = new Subject<void>();
 
@@ -34,13 +37,58 @@ export class GameDetailsComponent implements OnInit, OnDestroy {
     this.route.paramMap.subscribe((params) => {
         this.gameId = params.get('id');
         if(this.gameId) {
-          this.httpService.getGameDetails(this.gameId).pipe(takeUntil(this.unsub)).subscribe(res => {
-            if(res) {
-              this.game = res;
+          this.fetchDetails();
+        }
+      })
+  }
+
+  fetchDetails() {
+    if(this.gameId) {
+
+      let assetSources: Observable<any>[] = [];
+
+      this.httpService.getGameDetails(this.gameId).subscribe(res => {
+        this.game = res;
+
+        if(this.game.movies_count > 0) {
+          assetSources.push(this.httpService.getGameTrailers(this.game.id).pipe(map(val => ({type: 'trailer', val: val}))));
+        }
+
+        if(this.game.screenshots_count > 0) {
+          assetSources.push(this.httpService.getGameScreenshots(this.game.id).pipe(map(val => ({type: 'screenshot', val: val}))));
+        }
+
+        if(assetSources.length > 0) {
+          forkJoin(assetSources).subscribe(results => {
+
+            let screenshots: Screenshot[] = [];
+            let trailers: Trailer[] = [];
+
+            for(let result of results) {
+              if(result.type === 'trailer') {
+                trailers = result.val.results;
+              }
+
+              if(result.type === 'screenshot') {
+                screenshots = result.val.results;
+              }
             }
+
+            this.tabsData = {game: this.game, screenshots: screenshots, trailers: trailers};
           });
         }
       })
+
+      // let sources = [this.httpService.getGameDetails(this.gameId),,]
+
+      // forkJoin(sources).pipe(map(([res1,res2,res3]) => {
+      //   return {game: res1, trailers: res2, screenshots: res3}
+      // })).subscribe(res => {
+      //   this.game = res.game as Game;
+      //   this.game.trailers = (res.trailers as ApiResponse<Trailer>).results;
+      //   this.game.screenshots = (res.screenshots as ApiResponse<Screenshot>).results;
+      // });
+    }
   }
 
   ngOnDestroy() {
